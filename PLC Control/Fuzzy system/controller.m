4b592ba73bf10f55c43c2e24bf3a36622dcbb927
Pablo Santervás <pablosantervasblanco@gmail.com>
2024-11-13 12:23:09 +0100

    Fix error in the model and first version of the control

--- PLC Control/Fuzzy system/controller.m
+++ PLC Control/Fuzzy system/controller.m
@@ -6,8 +6,7 @@ function [u] = controller(error, vel, theta)
     error = round(error);
     vel = round(vel);
     theta = round(theta);
-    write(m, 'holdingregs', 1, [error, vel, theta], 'int16'); %write in the PLC
-    pause(0.01);
-    ui = read(m, 'holdingregs', 101, 1,'int16'); %read of the PLC
-    u = ui / 100;
+    write(m, 'holdingregs', 501, [error, vel, theta], 'int16'); %write in the PLC
+    pause(0.1);
+    u = read(m, 'holdingregs', 504, 1,'int16'); %read of the PLC
 end
